bec24cb161f48ec4089428beb3fc0d755ebe1cf0
orkonomics <hoag.jeff@gmail.com>
2026-07-24 16:14:27 -0700

    they can know what things are theirs
    pushing item detection for characters now :)

--- gmtk-game-jam-2026-count-down/Scripts/Globals.gd
+++ gmtk-game-jam-2026-count-down/Scripts/Globals.gd
@@ -22,10 +22,11 @@ signal trans(target: String)
 signal states(state)
 
 #shit jeff added
-var inventory = ["Hefty Knife", "Ripped Nets"] #player's inventory relative to what they collected
+var inventory = [""] #player's inventory relative to what they collected
 var item1 = ""
 var item2 = ""
 var item3 = ""
+var item4 = ""
 
 func dm_print( p ): #print function so I can print from the dialogue manager
 	print(p)
