ae03920c3e3c299a7e8cf64f90dc1382198972b6
broskiiiadactyl <zandersfong@gmail.com>
2026-07-26 09:30:36 -0400

    intro screen

--- gmtk-game-jam-2026-count-down/Scripts/Start.gd
+++ gmtk-game-jam-2026-count-down/Scripts/Start.gd
@@ -19,9 +19,35 @@ func _on_button_pressed() -> void:
 	%Load.visible = true
 
 func turn_off() -> void:
+	var tween1 = create_tween()
+	tween1.tween_property(%JMenu, "modulate", Color(0,0,0,0.0), 0.5)
+	await tween1.finished
+	
+	var tween2 = create_tween()
+	tween2.tween_property(%Hint, "modulate", Color(1,1,1,1.0), 0.5)
+	await tween2.finished
+	
+	await get_tree().create_timer(2.0).timeout
+	
+	var tween3 = create_tween()
+	tween3.tween_property(%Hint, "modulate", Color(1,1,1,0.0), 0.5)
+	await tween3.finished
+	
+	var tween4 = create_tween()
+	tween4.tween_property(%Hint2, "modulate", Color(1,1,1,1.0), 0.5)
+	await tween4.finished
+	
+	await get_tree().create_timer(2.0).timeout
+	
+	var tween5 = create_tween()
+	tween5.tween_property(%Hint2, "modulate", Color(1,1,1,0.0), 0.5)
+	await tween5.finished
+	
 	Globals.set_active_state(Globals.gamestate.MOVE)
 	var tween = create_tween()
 	tween.tween_property(pause_menu, "modulate", Color(0,0,0,0), 0.25)
+	tween.parallel().tween_property(%HintScreen, "modulate", Color(0,0,0,0), 0.25)
 	await tween.finished
 	pause_menu.visible = false
+	%HintScreen.visible = false
 	self.process_mode = Node.PROCESS_MODE_DISABLED
